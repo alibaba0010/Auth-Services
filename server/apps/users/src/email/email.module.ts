73a02188b87da62efe4d73f64f7a50f0151e7965
alibaba0010 <zakariyahali100@gmail.com>
2024-11-30 17:34:48 +0100

    adding email module to the project

--- server/apps/users/src/email/email.module.ts
+++ server/apps/users/src/email/email.module.ts
@@ -1,7 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+@Global()
 @Module({
-  providers: [EmailService]
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('EMAIL_HOST'),
+          port: config.get('MAIL_PORT'),
+          secure: true,
+          auth: {
+            user: config.get('EMAIL_USER'),
+            pass: config.get('EMAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: 'alibabadeliveryapp.com',
+        },
+        template: {
+          dir: join(__dirname, '../../../email-templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [EmailService],
 })
 export class EmailModule {}
--- server/apps/users/src/main.ts
+++ server/apps/users/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
+  const app = await NestFactory.create<NestExpressApplication>(UsersModule);
+  // app.useStaticAssets(join(__dirname, '../../email-templates'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'server/email-templates'));
+  app.setViewEngine('ejs');
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
