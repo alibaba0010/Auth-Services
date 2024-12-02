15a5e2b9fbd944c538bd5a6b7ed0b2d29cfa5726
alibaba0010 <zakariyahali100@gmail.com>
2024-12-02 15:01:44 +0100

    Adding nextui

---
+++ client/src/app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Home from "@/src/views/Home";
+
+const Page = (props) => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+Page.propTypes = {};
+
+export default Page;
---
+++ client/src/components/Layouts/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = (props) => {
+  return <header>Header</header>;
+};
+
+Header.propTypes = {};
+
+export default Header;
