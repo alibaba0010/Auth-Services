1146c4bbdc97c1d862d25208a1331956ad315757
alibaba0010 <zakariyahali100@gmail.com>
2024-12-04 23:03:08 +0100

    Adding new views

--- client/src/views/Auth.tsx
+++ client/src/views/Auth.tsx
@@ -3,13 +3,19 @@ import Login from "../app/auth/Login";
 import Register from "../app/auth/Register";
 import Verification from "../app/auth/Verifications";
 
-const Auth = () => {
+const Auth = ({ setOpen }: { setOpen: (e: boolean) => void }) => {
   const [activeState, setActiveState] = useState("Login");
 
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target instanceof HTMLDivElement && e.target.id === "screen") {
+      setOpen(false);
+    }
+  };
   return (
     <div
       className="w-full fixed top-0 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]"
       id="screen"
+      onClick={handleClose}
     >
       <div className="w-[500px] bg-slate-900 rounded shadow-sm p-3">
         {activeState === "Login" && <Login setActiveState={setActiveState} />}
