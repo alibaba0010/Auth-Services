d9103b75bafdf6e20eef7c661d3656b86f98473a
alibaba0010 <zakariyahali100@gmail.com>
2024-12-04 17:08:21 +0100

    modified the login actions logic

--- client/src/graphql/actions/login.actions.ts
+++ client/src/graphql/actions/login.actions.ts
@@ -2,7 +2,7 @@ import { DocumentNode, gql } from "@apollo/client";
 
 export const LOGIN_USER: DocumentNode = gql`
   mutation LoginUser($email: String!, $password: String!) {
-    loginUser(email: $email, password: $password) {
+    loginUser(loginInput: { email: $email, password: $password }) {
       user {
         id
         name
