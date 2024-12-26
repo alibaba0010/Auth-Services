dad7cd7d8c8f66981f910b6dde8e749d49928450
alibaba0010 <zakariyahali100@gmail.com>
2024-12-26 09:18:32 +0100

    Debugging error checking if useruserExists

--- client/src/actions/registerUser.ts
+++ client/src/actions/registerUser.ts
@@ -1,10 +1,16 @@
 "use server";
+
+import prisma from "../lib/prismaDB";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const registerUser = async (user: any) => {
-  console.log("User registered: ", user);
   const { name, email } = user;
-  const newUser = await prismadb.users.create({
+  const userExists = await prisma.users.findUnique({ where: { email } });
+  if (userExists) {
+    return userExists;
+  }
+  const newUser = await prisma.users.create({
     data: { name, email },
   });
-  return { newUser };
+  return newUser;
 };
