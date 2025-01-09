e764e5c3799386b7cd0782d529de27068256fc64
alibaba0010 <zakariyahali100@gmail.com>
2025-01-09 12:33:06 +0100

    error in reset-passwordtring to deploy to vercel

--- client/src/app/(routes)/reset-password/page.tsx
+++ client/src/app/(routes)/reset-password/page.tsx
@@ -3,15 +3,21 @@ import React, { FC } from "react";
 import ResetPassword from "../../auth/ResetPassword";
 
 interface SearchParamsProps {
-  searchParams: {
+  searchParams: Promise<{
     // [key: string]: string | string[] | undefined;
     [key: string]: string | undefined;
-  };
+  }>;
 }
 const Reset_Password: FC<SearchParamsProps> = ({
   searchParams,
 }): React.ReactElement => {
-  const token = (searchParams["verify"] as string) ?? "";
+  const [token, setToken] = React.useState<string>("");
+
+  React.useEffect(() => {
+    searchParams.then((params) => {
+      setToken(params["verify"] ?? "");
+    });
+  }, [searchParams]);
 
   // const token = searchParams["verify"] ?? "";
   console.log("Token: " + token);
