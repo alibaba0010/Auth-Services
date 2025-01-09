76c4c33e6080781c9ff9e2a0b9c470d3f29fbc2b
alibaba0010 <zakariyahali100@gmail.com>
2025-01-09 01:31:40 +0100

    solving CSRF error in apollo

--- server/apps/users/src/main.ts
+++ server/apps/users/src/main.ts
@@ -2,13 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(UsersModule);
   // app.useStaticAssets(join(__dirname, '../../email-templates'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'server/email-templates'));
   app.setViewEngine('ejs');
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
+
   app.enableCors({ origin: '*' });
   await app.listen(process.env.port ?? 3001);
 }
