9212e35273b807b80449bcb89b7068475c9f7b2f
alibaba0010 <zakariyahali100@gmail.com>
2025-01-09 02:08:35 +0100

    removing helmet

--- server/apps/users/src/main.ts
+++ server/apps/users/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(UsersModule);
   // app.useStaticAssets(join(__dirname, '../../email-templates'));
